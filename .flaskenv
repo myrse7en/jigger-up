05a877bcf2a43ec76792eb098289b7d4116775a6
Mayur Bhatia <23645213+myrse7en@users.noreply.github.com>
2019-01-09 23:35:29 -0500

    Login form rendering and accepting input

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,2 @@
-FLASK_APP=microblog.py
+FLASK_APP=main.py
 FLASK_DEBUG=1
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,36 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Myr'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day today in Boston!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'That\'s a cool website!'
+        },
+        {
+            'author': {'username': 'Jeff'},
+            'body': 'Massage me.'
+        },
+        {
+            'author': {'username': 'Connor'},
+            'body': 'Chop Chop!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
